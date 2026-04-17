dab08c49ff54fedd372c0ae99316219ddb12fa0e
Arthur <gamerjuanma9@gmail.com>
2026-04-17 06:42:19 -0300

    Primeros pasos del bot

---
+++ main.py
@@ -0,0 +1,24 @@
+import discord
+import os
+from dotenv import load_dotenv
+
+# 1. Cargar las variables secretas del archivo .env
+load_dotenv()
+TOKEN = os.getenv('TOKEN_DISCORD')
+
+# 2. Configurar los permisos (intents)
+intents = discord.Intents.default()
+# Habilitamos el intent de lectura para cuando hagamos el bot de "Gay el de abajo"
+intents.message_content = True 
+
+# 3. Crear el cliente del bot
+cliente = discord.Client(intents=intents)
+
+# 4. El sensor de encendido (Evento on_ready)
+@cliente.event
+async def on_ready():
+    print(f'¡Estatus: Conectado exitosamente como {cliente.user}!')
+    print('El 1% de hoy está completado. 🚀')
+
+# 5. Encender el bot usando la variable oculta
+cliente.run(TOKEN)
